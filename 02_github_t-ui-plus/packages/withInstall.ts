256e116400abf814fc9850f52c6866c861a6f991
zhangping <2425238116@qq.com>
2024-07-17 18:02:33 +0800

    chore: 添加注释

--- 02_github_t-ui-plus/packages/withInstall.ts
+++ 02_github_t-ui-plus/packages/withInstall.ts
@@ -3,6 +3,7 @@ import { App, Plugin } from 'vue';
 type SFCWithInstall<T> = T & Plugin;
 
 export const withInstall = <T, E extends Record<string, any>>(main: T, extra?: E) => {
+  // 在项目中的main.js中，有人用vue.use(button)去使用而准备的
   (main as SFCWithInstall<T>).install = (app: App) => {
     // 单独注册组件为全局组件（用于按需引入）
     for (const comp of [main, ...Object.values(extra ?? {})]) {
