74f7a9a0b926a598548dff60125b76d454d4bb93
zhangping <2425238116@qq.com>
2024-07-19 13:38:22 +0800

    chore: 添加备注

--- 02_github_t-ui-plus/packages/withInstall.ts
+++ 02_github_t-ui-plus/packages/withInstall.ts
@@ -3,9 +3,9 @@ import { App, Plugin } from 'vue';
 type SFCWithInstall<T> = T & Plugin;
 
 export const withInstall = <T, E extends Record<string, any>>(main: T, extra?: E) => {
-  // 在项目中的main.js中，有人用vue.use(button)去使用而准备的
+  // 在项目中的main.js中，使用 vue.use(ZpButton) 去注册插件
   (main as SFCWithInstall<T>).install = (app: App) => {
-    // 单独注册组件为全局组件（用于按需引入）
+    // 注册组件为全局组件
     for (const comp of [main, ...Object.values(extra ?? {})]) {
       app.component(comp.name, comp);
     }
