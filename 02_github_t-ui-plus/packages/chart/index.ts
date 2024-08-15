164838918b6c0fc3994e03b946be4d537e9e0827
zhangping <2425238116@qq.com>
2024-08-15 14:03:07 +0800

    feat: 去掉多余代码

--- 02_github_t-ui-plus/packages/chart/index.ts
+++
@@ -1,5 +0,0 @@
-import Chart from "./src/index.vue"
-import { withInstall } from "../withInstall"
-
-const TChart = withInstall(Chart)
-export default TChart
--- 02_github_t-ui-plus/packages/select/index.ts
+++
@@ -1,5 +0,0 @@
-import Select from "./src/index.vue"
-import { withInstall } from "../withInstall"
-
-const TSelect = withInstall(Select)
-export default TSelect
