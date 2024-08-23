14e2a89ec298e1f9717e28af5d15a51a0488d2ee
zhangping <2425238116@qq.com>
2024-08-23 16:41:31 +0800

    feat: t-测试

--- t-ui-01/packages/button/src/index.vue
+++ t-ui-01/packages/button/src/index.vue
@@ -1,41 +1,6 @@
 <template>
-  <el-tooltip v-if="tip" :content="tip" :placement="placement" v-bind="tipProps">
-    <el-button v-bind="$attrs" class="t-button-tip" @click="handleClick">
-      <slot />
-    </el-button>
-  </el-tooltip>
-  <el-button v-else v-bind="$attrs" @click="handleClick"> 前缀1+<slot /> </el-button>
+  <el-button> 前缀1+<slot /> </el-button>
 </template>
 
-<script setup lang="ts" name="TButton">
-import { ref } from "vue"
-const props = defineProps({
-  time: {
-    type: Number,
-    default: 1000
-  },
-  tip: {
-    type: String,
-    default: ""
-  },
-  placement: {
-    type: String,
-    default: "top"
-  },
-  tipProps: {
-    type: Object,
-    default: () => ({})
-  }
-})
-// 抛出事件
-const emits = defineEmits(["click"])
-const record = ref(0)
-const handleClick = () => {
-  let newTime = new Date()
-  if (newTime.getTime() - record.value > props.time) {
-    emits("click")
-  }
-  record.value = new Date().getTime()
-}
-</script>
+<script setup lang="ts" name="TButton"></script>
 <style lang="scss" scoped></style>
